b97ee77587e7c1e5cacd7ac855a43ab589ea0cce
Jakub Smékal <jakub.smekal@gmail.com>
2022-03-13 09:32:33 +0000

    Added graph actinf

--- blockference/gridference.py
+++ blockference/gridference.py
@@ -88,3 +88,70 @@ def actinf_planning_single(agent, env_state, A, B, C, prior):
             'update_env': current_state,
             'update_action': chosen_action,
             'update_inference': qs_current}
+
+def actinf_graph(agent_network):
+
+    # list of all updates to the agents in the network
+    agent_updates = []
+
+    for agent in agent_network.nodes:
+
+        policies = construct_policies([agent_network.nodes[agent]['agent'].n_states], [len(agent_network.nodes[agent]['agent'].E)], policy_len = agent_network.nodes[agent]['agent'].policy_len)
+        # get obs_idx
+        obs_idx = grid.index(agent_network.nodes[agent]['env_state'])
+
+        # infer_states
+        qs_current = u.infer_states(obs_idx, agent_network.nodes[agent]['prior_A'], agent_network.nodes[agent]['prior'], noise=1)
+
+        # calc efe
+        _G = u.calculate_G_policies(agent_network.nodes[agent]['prior_A'], agent_network.nodes[agent]['prior_B'], agent_network.nodes[agent]['prior_C'], qs_current, policies=policies)
+
+        # calc action posterior
+        Q_pi = u.softmax(-_G)
+        # compute the probability of each action
+        P_u = u.compute_prob_actions(agent_network.nodes[agent]['agent'].E, policies, Q_pi)
+        
+        # sample action
+        chosen_action = u.sample(P_u)
+
+        # calc next prior
+        prior = agent_network.nodes[agent]['prior_B'][:,:,chosen_action].dot(qs_current) 
+
+        # update env state
+        # action_label = params['actions'][chosen_action]
+
+        (Y, X) = agent_network.nodes[agent]['env_state']
+        Y_new = Y
+        X_new = X
+        # here
+
+        if chosen_action == 0: # UP
+            
+            Y_new = Y - 1 if Y > 0 else Y
+            X_new = X
+
+        elif chosen_action == 1: # DOWN
+
+            Y_new = Y + 1 if Y < agent_network.nodes[agent]['agent'].border else Y
+            X_new = X
+
+        elif chosen_action == 2: # LEFT
+            Y_new = Y
+            X_new = X - 1 if X > 0 else X
+
+        elif chosen_action == 3: # RIGHT
+            Y_new = Y
+            X_new = X +1 if X < agent_network.nodes[agent]['agent'].border else X
+
+        elif chosen_action == 4: # STAY
+            Y_new, X_new = Y, X 
+            
+        current_state = (Y_new, X_new) # store the new grid location
+        agent_update = {'source': agent,
+                        'update_prior': prior,
+                        'update_env': current_state,
+                        'update_action': chosen_action,
+                        'update_inference': qs_current}
+        agent_updates.append(agent_update)
+
+    return {'agent_updates': agent_updates}
