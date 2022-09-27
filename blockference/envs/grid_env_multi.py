fc54977e8b3e8e5c37393c59bf609ec4be8d36d0
Jakub Smékal <jakub.smekal@gmail.com>
2022-09-27 08:27:05 +0100

    WIP: refactored step function in gridenv

--- blockference/envs/grid_env_multi.py
+++ blockference/envs/grid_env_multi.py
@@ -15,8 +15,11 @@ class GridAgent():
         grid = list(itertools.product(range(3), repeat=2))
         self.border = np.sqrt(len(grid)) - 1
         self.pos_dict = {}
+        print(f'pos-dict is {self.pos_dict}')
+        assert 1==0
         for i in range(0, len(grid)):
             self.pos_dict[i] = grid[i]
+
         self.grid_dim = grid_dim
         self.no_actions = 2 * grid_dim + 1
         self.n_observations = grid_len ** 2
@@ -28,22 +31,21 @@ class GridAgent():
         assert len(self.states) == len(agents)
 
     def step(self, actions):
-        # assert len(self.states) == len(actions), "Number of actions received is more than number of agents"
-        print(f"actions received: {actions} with length: {len(actions)}")
-        print(f"actions for agent 1: {self.E[int(actions[0])]} and for agent 2: {self.E[int(actions[1])]}")
-        print(f"Current state is: {self.states}")
-        print(f"Current observations are (agent locations): {self.agent_locs}")
         next_state = copy.deepcopy(self.states)
         
         for idx, action in enumerate(actions):
+            # get indexes of the current reference agent and the other agent (2-agent case, in the future might be handled with a dict)
             agent_idx = idx
             other_agent_idx = 0 if agent_idx == 1 else 1
-            new_loc = copy.deepcopy(self.states[idx][0]) # new location of agent on grid
-            new_ref = copy.deepcopy(self.states[idx][1]) # new relative position to the other agent on the grid
+            
+            # initialize new agent state
+            new_agent_state = copy.deepcopy(self.states[agent_idx]) # new location of agent on grid
+            other_agent_state = self.states[other_agent_idx]
+            
+            # get word action label
             action_label = self.E[int(action[0])]
-            # y, x = self.states[idx][0] # looks like [1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
-            k = [k for k, i in enumerate(self.states[idx][0]) if i != 0]
-            y, x = self.pos_dict[k[0]]
+
+            y, x = self.pos_dict[agent_idx]
 
             if action_label == "DOWN":
                 next_y = y - 1 if y > 0 else y
@@ -60,19 +62,16 @@ class GridAgent():
             elif action_label == "STAY":
                 next_x = x
                 next_y = y
+
             new_location = (next_y, next_x)
-            try:
-                rel_pos = self.get_rel_pos(new_location, self.states[idx+1][0])
-            except:
-                rel_pos = self.get_rel_pos(new_location, self.states[idx-1][0])
-            if rel_pos == "COLLISION":
-                new_location = self.states[idx][0]
-                next_state = (self.grid.index(new_location), new_ref)
-            else:
-                new_ref = self.rel_locs.index(rel_pos)
-                next_state[idx] = (self.grid.index(new_location), new_ref)
-            self.agent_locs[idx] = new_location
-        return next_state # update both agents at the same time, need to be optimized in future iterations
+            new_agent_state = list(mydict.keys())[list(mydict.values()).index(new_location)
+            
+            # check for collisions
+            if new_agent_state == other_agent_state:
+                new_agent_state = self.states[agent_idx] # i.e. could not perform the action
+            self.states[agent_idx] = new_agent_state # update state
+
+        return self.states # update both agents at the same time, need to be optimized in future iterations
 
     def is_collision(self, agent_idx, other_agent_idx, new_location):
 
